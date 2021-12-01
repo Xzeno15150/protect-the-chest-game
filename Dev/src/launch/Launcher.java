221d8a29a5b8d1a14710e0530b5c07dcdb6bc0b9
vigaillard2 <victor.gaillard@etu.uca.fr>
2021-12-01 13:52:29 +0100

    Configuration du projet + ajout de la première Vue

--- Dev/src/launch/Launcher.java
+++ Dev/src/launch/Launcher.java
@@ -1,4 +1,16 @@
 package launch;
 
-public class Launcher extends Application{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent racine = FXMLLoader.load(getClass().getResource("/fxml/MainWindow.fxml"));
+        primaryStage.setScene(new Scene(racine));
+        primaryStage.show();
+    }
 }
---
+++ Dev/src/views/MainWindow.java
@@ -0,0 +1,11 @@
+package views;
+
+import javafx.fxml.FXML;
+
+public class MainWindow {
+
+    @FXML
+    public void initialize() {
+
+    }
+}
